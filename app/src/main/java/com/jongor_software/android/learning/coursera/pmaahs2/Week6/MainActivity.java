016172c0818c528415c5877f9177bd781fcaad1a
Jon Gordon <jon@jongor-software.com>
2015-05-22 23:24:59 +0100

    Starte dWeek 6 - Notifications and Broadcast Receivers

---
+++ app/src/main/java/com/jongor_software/android/learning/coursera/pmaahs2/Week6/MainActivity.java
@@ -0,0 +1,9 @@
+package com.jongor_software.android.learning.coursera.pmaahs2.Week6;
+
+import android.app.Activity;
+
+/**
+ * Created by jon on 22/05/15.
+ */
+public class MainActivity extends Activity implements SelectionListener, DownloadFinishedListener {
+}
